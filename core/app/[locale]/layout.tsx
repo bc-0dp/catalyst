b4649ef89431f4b14721fac0174752fbe44865f2
bc-0dp <johan.nielsen@bigcommerce.com>
2024-12-06 12:02:56 +0100

    Le Buyer Portal Experience

--- core/app/[locale]/layout.tsx
+++ core/app/[locale]/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from 'next/font/google';
 import { NextIntlClientProvider, useMessages } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
 import { PropsWithChildren } from 'react';
+import Script from 'next/script';
+import Head from 'next/head';
 
 import '../globals.css';
 
@@ -90,11 +92,44 @@ export default function RootLayout({ children, params: { locale } }: Props) {
   return (
     <html className={`${inter.variable} font-sans`} lang={locale}>
       <body className="flex h-screen min-w-[375px] flex-col">
+        <Head>
+          <link rel="preload" href="http://localhost:3001/src/headless.ts" as="script" />
+          <script type="module">
+            {`
+          import RefreshRuntime from 'http://localhost:3001/@react-refresh'
+          RefreshRuntime.injectIntoGlobalHook(window)
+          window.$RefreshReg$ = () => {}
+          window.$RefreshSig$ = () => (type) => type
+          window.__vite_plugin_react_preamble_installed__ = true
+          `}
+          </script>
+          <script type="module" src="http://localhost:3001/@vite/client"></script>
+        </Head>
         <Notifications />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Providers>{children}</Providers>
         </NextIntlClientProvider>
         <VercelComponents />
+
+        <a href="/#/login">Bob</a>
+
+        <Script id="b3-settings">
+          {`window.B3 = {
+            setting: {
+              store_hash: 'n3u8oc5vgz', 
+              channel_id: 1680735,
+              platform: 'catalyst',
+            },
+          'dom.registerElement':
+          '[href^="/login"]',            
+          };`}
+        </Script>
+        <Script
+          data-channelid="1680735"
+          data-storehash="n3u8oc5vgz"
+          src="http://localhost:3001/src/headless.ts"
+          type="module"
+        />
       </body>
     </html>
   );
