cd6eba80e5c54147b7134fbbe5e3f4a2ccb3a113
bc-nick <nick.tsybulko@bigcommerce.com>
2025-02-26 23:50:29 +0100

    feat(payment): PAYPAL-5114 headless wallet buttons implementation

--- core/app/[locale]/(default)/cart/_actions/redirect-to-checkout.ts
+++ core/app/[locale]/(default)/cart/_actions/redirect-to-checkout.ts
@@ -1,16 +1,8 @@
 'use server';
 
-import { BigCommerceGQLError } from '@bigcommerce/catalyst-client';
-import { SubmissionResult } from '@conform-to/react';
-import { parseWithZod } from '@conform-to/zod';
-import { getLocale, getTranslations } from 'next-intl/server';
-import { z } from 'zod';
-
 import { getSessionCustomerAccessToken } from '~/auth';
 import { client } from '~/client';
 import { graphql } from '~/client/graphql';
-import { redirect } from '~/i18n/routing';
-import { getCartId } from '~/lib/cart';
 
 const CheckoutRedirectMutation = graphql(`
   mutation CheckoutRedirectMutation($cartId: String!) {
@@ -24,24 +16,8 @@ const CheckoutRedirectMutation = graphql(`
   }
 `);
 
-export const redirectToCheckout = async (
-  _lastResult: SubmissionResult | null,
-  formData: FormData,
-): Promise<SubmissionResult | null> => {
-  const locale = await getLocale();
-  const t = await getTranslations('Cart.Errors');
-
+export const redirectToCheckout = async (cartId: string) => {
   const customerAccessToken = await getSessionCustomerAccessToken();
-
-  const submission = parseWithZod(formData, { schema: z.object({}) });
-
-  const cartId = await getCartId();
-
-  if (!cartId) {
-    return submission.reply({ formErrors: [t('cartNotFound')] });
-  }
-
-  let url;
 
   try {
     const { data } = await client.fetch({
@@ -51,27 +27,11 @@ export const redirectToCheckout = async (
       customerAccessToken,
     });
 
-    url = data.cart.createCartRedirectUrls.redirectUrls?.redirectedCheckoutUrl;
+    return data;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
 
-    if (error instanceof BigCommerceGQLError) {
-      return submission.reply({
-        formErrors: error.errors.map(({ message }) => message),
-      });
-    }
-
-    if (error instanceof Error) {
-      return submission.reply({ formErrors: [error.message] });
-    }
-
-    return submission.reply({ formErrors: [t('failedToRedirectToCheckout')] });
+    throw error;
   }
-
-  if (!url) {
-    return submission.reply({ formErrors: [t('failedToRedirectToCheckout')] });
-  }
-
-  return redirect({ href: url, locale });
 };
